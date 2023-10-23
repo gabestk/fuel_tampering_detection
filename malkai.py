1317d59f98a60d839b8b317d554e6d7f1f29e25a
Gabriel Trajano <131036391+gabestk@users.noreply.github.com>
2023-10-23 03:18:51 -0300

    update

--- malkai.py
+++ malkai.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
+
 
 # Sumo configuration imports
 import os
@@ -321,12 +322,10 @@ def run():
         # Function that writes a json file containing the data from all the vehicle
         #malkai(sorted_data)
         gabriel(sorted_fuel_data)
-
+        
         # Carrega os dados
         data = pd.read_json('vehicle_fueling_file.json')
-        #data['diff_refuel_liters'] = data['expected_refuel_liters'] - data['real_refuel_liters']
-        #data['diff_fuel_percentage'] = data['expected_fuel_percentage'] - data['real_fuel_percentage']
-        
+
         # Seleciona os recursos que serão usados para a previsão
         features = ['vehicle_factory_error', 'vehicle_random_error', 'real_refuel_liters', 'real_fuel_percentage', 'expected_fuel_percentage', 'expected_refuel_liters', 'refuel_amount_liters']
         target = 'fraud'
@@ -334,46 +333,66 @@ def run():
         # Divide os dados em conjuntos de treinamento e teste
         X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2)
 
-        # Variante com a fraude fixa em 8% e os erros aleatórios
-        X_train_noisy_fraud_fixed = X_train.copy()
-        X_train_noisy_fraud_fixed['fraud_liters'] = 4
+        # Aplica SMOTE para oversampling da classe minoritária
+        smote = SMOTE()
+        X_train, y_train = smote.fit_resample(X_train, y_train)
 
-        X_test_noisy_fraud_fixed = X_test.copy()
-        X_test_noisy_fraud_fixed['fraud_liters'] = 4
-
-        # Variante com o erro fixo em 8% e a fraude aleatória
+        # Variante com o erro fixo em 5% e a fraude aleatória
         X_train_noisy_error_fixed = X_train.copy()
-        X_train_noisy_error_fixed['vehicle_factory_error'] = 2
-        X_train_noisy_error_fixed['vehicle_random_error'] = 2
+        X_train_noisy_error_fixed['vehicle_factory_error'] = 1
+        X_train_noisy_error_fixed['vehicle_random_error'] = 1.5
 
         X_test_noisy_error_fixed = X_test.copy()
-        X_test_noisy_error_fixed['vehicle_factory_error'] = 2
-        X_test_noisy_error_fixed['vehicle_random_error'] = 2
+        X_test_noisy_error_fixed['vehicle_factory_error'] = 1
+        X_test_noisy_error_fixed['vehicle_random_error'] = 1.5
 
         # Normaliza os dados
         scaler = StandardScaler()
 
-        variants = ['original', 'fraud_fixed', 'error_fixed']
+        fraud_levels = [0.05, 0.075, 0.10, 0.125, 0.15]
+
         variant_data = {
             'original': (X_train, X_test),
-            'fraud_fixed': (X_train_noisy_fraud_fixed, X_test_noisy_fraud_fixed),
             'error_fixed': (X_train_noisy_error_fixed, X_test_noisy_error_fixed)
         }
 
-        for variant in variants:
+        for fraud_level in fraud_levels:
+            X_train_noisy_fraud_level = X_train_noisy_error_fixed.copy()
+            X_test_noisy_fraud_level = X_test_noisy_error_fixed.copy()
+            X_train_noisy_fraud_level['refuel_amount_liters'] *= (1 - fraud_level)
+            X_test_noisy_fraud_level['refuel_amount_liters'] *= (1 - fraud_level)
+            variant_data[f'error_fixed_fraud_{int(fraud_level * 100)}'] = (X_train_noisy_fraud_level, X_test_noisy_fraud_level)
+
+        plt.figure(figsize=(10, 10))
+
+        for variant in variant_data.keys():
             X_train_variant, X_test_variant = variant_data[variant]
-            
+
+            if 'fraud' in variant:
+                fraud_level = int(variant.split('_')[-1]) / 100
+            else:
+                fraud_level = 0
+
             X_train_scaled = scaler.fit_transform(X_train_variant)
             X_test_scaled = scaler.transform(X_test_variant)
 
-            # Crie e treine o modelo com validação cruzada e ajuste de hiperparâmetros
             model = RandomForestClassifier()
-            param_grid = {'n_estimators': [100, 200, 300], 'max_depth': [None, 5, 10], 'min_samples_split': [2, 5]}
+            param_grid = {'n_estimators': [100, 200, 300], 'max_depth': [None, 5, 10], 'min_samples_split': [2, 5], 'min_samples_leaf': [1, 2], 'bootstrap': [True, False]}
             grid_search = GridSearchCV(model, param_grid, cv=5)
             grid_search.fit(X_train_scaled, y_train)
 
             # Faça previsões no conjunto de teste
+            y_pred_prob = grid_search.predict_proba(X_test_scaled)[:,1]
             y_pred = grid_search.predict(X_test_scaled)
+
+            # Calcule a curva ROC
+            fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+            
+            # Calcule a AUC
+            roc_auc = auc(fpr, tpr)
+
+            # Plote a curva ROC para cada nível de fraude
+            plt.plot(fpr, tpr, label=f'Fraud Level {fraud_level} (AUC = {roc_auc:.2f})')
 
             # Crie a matriz de confusão
             cm = confusion_matrix(y_test, y_pred)
@@ -381,9 +400,8 @@ def run():
             # Exiba a matriz de confusão
             plt.figure(figsize=(10, 7))
             sns.heatmap(cm, annot=True, fmt='d')
-            plt.title('Confusion Matrix for ' + variant)
-            plt.savefig('confusion_matrix_' + variant + '.png')
-            plt.show()
+            plt.title(f'Confusion Matrix for {variant} (Fraud: {fraud_level * 100}%)')
+            plt.savefig(f'confusion_matrix_{variant}_fraud_{int(fraud_level * 100)}.png')
 
             # Gere e exiba o relatório de classificação
             report = classification_report(y_test, y_pred, output_dict=True)
@@ -391,10 +409,18 @@ def run():
 
             plt.figure(figsize=(10, 7))
             sns.heatmap(df_report.iloc[:-1, :-1], annot=True)
-            plt.title('Classification Report for ' + variant)
-            plt.savefig('classification_report_' + variant + '.png')
-            plt.show()
+            plt.title(f'Classification Report for {variant} (Fraud: {fraud_level * 100}%)')
+            plt.savefig(f'classification_report_{variant}_fraud_{int(fraud_level * 100)}.png')
 
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+        plt.savefig('Grafico.png')
+                
     finally:
 
         # Close the simulation
